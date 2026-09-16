3efc2b8ed2e8c1e8059b1801d330a0bac7eef2fb
Kevin Lopez <kevindeveloper07@gmail.com>
2026-09-15 20:50:11 -0500

    configurar almacenamiento de imagenes

--- core/settings.py
+++ core/settings.py
@@ -43,8 +43,6 @@ INSTALLED_APPS = [
 ]
 
 # Configuración de archivos multimedia (Imágenes de productos)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
